ec0acf7e3dad683cb7983aa774d572d9b0128d4f
Kuldip Kumar <kkvkicktheworldup7@gmail.com>
2017-04-01 00:13:44 +0530

    First Commit

--- MdrTV.py
+++ MdrTV.py
@@ -1,17 +1,22 @@
+	              ####Python Script to download the recent video of MadridistaTV channel of youtube####
+
 import requests
 import bs4 as bs
 
 url = 'https://www.youtube.com/user/elMadridistaTV/videos'
+	                          	####To get the youtube link of the video#### 
 
 req = requests.get(url)
 soup = bs.BeautifulSoup(req.content,'lxml')
-
 vidoes = soup.find('div',{'class':'branded-page-v2-body branded-page-v2-primary-column-content'})
 vlist = vidoes.find('ul',{'id':'browse-items-primary'}).find('ul',{'class':'channels-browse-content-grid branded-page-gutter-padding grid-lockups-container'})
 video = vlist.find('li').find('div',{'class':'yt-lockup-content'}).find('a')
 videolink = video.get('href')
 
+					####To get the download link of the video####
+
 dwnpage_link = 'https://www.keepvid.com/?url=https://www.youtube.com{}'.format(videolink)
+
 req2 = requests.get(dwnpage_link)
 soup2 = bs.BeautifulSoup(req2.content,'lxml')
 title = soup2.find('div',{'id':'info'}).find('div',{'class':'text'}).find('h3').text
@@ -24,7 +29,9 @@ if(quality == '480p'):
 if(quality == '240p'):
 	link = Qualities[2].find('a').get('href')
 if(quality == '144p'):
-	link = Qualities[3].find('a').get('href')			
+	link = Qualities[3].find('a').get('href')	
+						####saving the file####
+
 title =title+'.mp4'
 req3 =requests.get(str(link))
 with open(title,'wb') as myfile:
